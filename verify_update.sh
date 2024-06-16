ba92de53907e6f203ae21eba4f732027a616883c
matinshiralikhan <shiralikhanamatin@gmail.com>
2024-06-16 15:58:52 +0330

    final test & commit bash script

--- verify_update.sh
+++ verify_update.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-LOG_FILE_PATH="/var/www/html/services/update-toolbox/app.log"
+LOG_FILE_PATH="/var/www/html/baadbaan-docker/services/update-toolbox/app.log"
 DATE=$(date +"%Y/%m/%d")
-echo -n > $LOG_FILE_PATH
-echo "Log file emptied."
-sleep 30
+FIRST_LOG_ENTRY=$(head -n 1 $LOG_FILE_PATH)
+
 if [[ "$FIRST_LOG_ENTRY" != *"$DATE"* || "$FIRST_LOG_ENTRY" != *"Started"* ]]; then
   echo "The first log entry is not from today or does not start with 'Started'"
   exit 1
